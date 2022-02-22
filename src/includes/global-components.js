bca4d2de536d73a100633dede8235f48a264d170
Erwin Errol Calanuga <hortsatta@gmail.com>
2022-02-22 16:55:21 +0800

    Add base components to global

--- src/includes/global-components.js
+++ src/includes/global-components.js
@@ -1,9 +1,17 @@
-import { BaseDivider, BaseDividerAlt, BaseFrame } from '@/components/base';
+import {
+  BaseButton,
+  BaseDivider,
+  BaseDividerAlt,
+  BaseFrame,
+  BaseSurface,
+} from '@/components/base';
 
 export default {
   install(app) {
+    app.component('BaseButton', BaseButton);
     app.component('BaseDivider', BaseDivider);
     app.component('BaseDividerAlt', BaseDividerAlt);
     app.component('BaseFrame', BaseFrame);
+    app.component('BaseSurface', BaseSurface);
   }
 };
