bb2649d0ce089406acb44a6f9bd3ff56318d61dd
Erwin Errol Calanuga <hortsatta@gmail.com>
2022-02-22 16:51:36 +0800

    Implement perfect scrollbar

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from 'vue';
+import PerfectScrollbar from 'vue3-perfect-scrollbar';
 
 import App from './App.vue';
 import router from './router';
@@ -7,11 +8,13 @@ import { buttonDirective } from './directives';
 import { globalComponents } from './includes';
 
 import './assets/css/tailwind.css';
+import 'vue3-perfect-scrollbar/dist/vue3-perfect-scrollbar.css';
 import './assets/css/main.css';
 
 createApp(App)
   .use(store)
   .use(router)
   .use(globalComponents)
+  .use(PerfectScrollbar)
   .directive('button', buttonDirective)
   .mount('#app');
