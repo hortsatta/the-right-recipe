e6dfd397357e9786cf9e1f0844e24f38f3f4f3c3
Erwin Errol Calanuga <hortsatta@gmail.com>
2022-02-23 17:46:02 +0800

    Add initial auth page

---
+++ src/views/AuthPage.vue
@@ -0,0 +1,38 @@
+<template>
+  <BaseFrame class="flex justify-center items-center">
+    <!-- Sign in -->
+    <div class="flex justify-center flex-1 w-1/2">
+      <BaseSurface class="w-[500px]">
+        <div class="pt-4">
+          <h2 class="px-5 py-3">Sign In</h2>
+          <BaseDivider />
+          <AuthFormSignIn class="px-5 pt-4 pb-10" />
+        </div>
+      </BaseSurface>
+    </div>
+    <div class="relative flex flex-col items-center flex-0 w-[100px] z">
+      <div class="w-px h-[100px] bg-border-400"></div>
+      <span class="text-xl font-display opacity-60">or</span>
+      <div class="w-px h-[100px] bg-border-400"></div>
+    </div>
+    <!-- Sign up -->
+    <div class="flex justify-center flex-1 w-1/2">
+      <BaseSurface class="w-[500px]">
+        <div class="pt-4">
+          <h2 class="px-5 py-3">Sign Up</h2>
+          <BaseDivider />
+          <AuthFormSignUp class="px-5 pt-4 pb-10" />
+        </div>
+      </BaseSurface>
+    </div>
+  </BaseFrame>
+</template>
+
+<script>
+import { AuthFormSignIn, AuthFormSignUp } from '@/components/auth';
+
+export default {
+  name: 'AuthPage',
+  components: { AuthFormSignIn, AuthFormSignUp }
+};
+</script>
