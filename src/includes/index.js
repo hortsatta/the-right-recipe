3f83693539f5c9decc6c21dada806352831a3b87
Erwin Errol Calanuga <hortsatta@gmail.com>
2022-02-24 20:44:45 +0800

    Implement firebase

--- src/includes/index.js
+++ src/includes/index.js
@@ -1,1 +1,2 @@
+export * from './firebase';
 export { default as globalComponents } from './global-components';
--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import router from './router';
 import store from './store';
 import { buttonDirective } from './directives';
 import { globalComponents } from './includes';
+import './includes/firebase';
 
 import './assets/css/tailwind.css';
 import 'vue3-perfect-scrollbar/dist/vue3-perfect-scrollbar.css';
