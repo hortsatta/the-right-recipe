84947e471b97757a9b6598ad5e5e43ca6ca8ad3c
Erwin Errol Calanuga <hortsatta@gmail.com>
2022-02-25 16:15:47 +0800

    Update template

--- src/components/auth/AuthFormSignIn.vue
+++ src/components/auth/AuthFormSignIn.vue
@@ -2,12 +2,13 @@
   <form @submit.prevent="handleSubmit">
     <!-- Fields -->
     <div class="grid grid-flow-row gap-y-4 pb-12">
-      <BaseInput label="Email" name="email" />
-      <BaseInput label="Password" name="password" />
+      <BaseInput type="email" label="Email" name="email" />
+      <BaseInput type="password" label="Password" name="password" />
     </div>
     <!-- Form controls -->
     <BaseButton
       class="w-full"
+      :loading="isSignInSubmitting"
       :disabled="isSubmitting"
       @click="handleSubmit"
     >
@@ -17,28 +18,37 @@
 </template>
 
 <script>
+import { computed } from 'vue';
+import { useStore } from 'vuex';
 import { useForm } from 'vee-validate';
 import * as yup from 'yup';
 
 export default {
   name: 'AuthFormSignIn',
-  setup() {
-    const initialValues = { email: '', password: '' };
+  setup: (_, ctx) => {
+    const store = useStore();
+
     const schema = yup.object({
       email: yup.string().required().email(),
       password: yup.string().required(),
     });
 
-    const { handleSubmit: submit, isSubmitting } = useForm({
+    const initialValues = { email: '', password: '' };
+
+    const { handleSubmit: submit } = useForm({
       validationSchema: schema,
       initialValues,
     });
 
     const handleSubmit = submit(async (data) => {
-      console.log(data);
+      ctx.emit('onSubmit', data);
     });
 
-    return { isSubmitting, handleSubmit };
+    return {
+      isSignInSubmitting: computed(() => store.state.auth.isSignInSubmitting),
+      isSubmitting: computed(() => store.getters['auth/isSubmitting']),
+      handleSubmit,
+    };
   },
 };
 </script>
--- src/components/auth/AuthFormSignUp.vue
+++ src/components/auth/AuthFormSignUp.vue
@@ -2,9 +2,9 @@
   <form @submit.prevent="handleSubmit">
     <!-- Fields -->
     <div class="grid grid-flow-row gap-y-4 pb-12">
-      <BaseInput label="Email" name="email" />
-      <BaseInput label="Password" name="password" />
-      <BaseInput label="Confirm Password" name="confirmPassword" />
+      <BaseInput type="email" name="email" label="Email" inputId="register-email" />
+      <BaseInput type="password" label="Password" name="password" inputId="register-password" />
+      <BaseInput type="password" label="Confirm Password" name="confirmPassword" />
       <hr class="mt-4 border-dashed border-border-200" />
       <BaseInput label="Name" name="displayName" />
     </div>
@@ -18,6 +18,7 @@
       </BaseButton>
       <BaseButton
         class="w-[200px]"
+        :loading="isSignUpSubmitting"
         :disabled="isSubmitting"
         @click="handleSubmit"
       >
@@ -28,12 +29,24 @@
 </template>
 
 <script>
+import { computed } from 'vue';
+import { useStore } from 'vuex';
 import { useForm } from 'vee-validate';
 import * as yup from 'yup';
 
 export default {
   name: 'AuthFormSignUp',
-  setup() {
+  setup: (_, ctx) => {
+    const store = useStore();
+
+    const schema = yup.object({
+      email: yup.string().required().email(),
+      password: yup.string().required().min(6),
+      confirmPassword: yup.string().required().oneOf([yup.ref('password')], 'password do not match.'),
+      displayName: yup.string().notRequired().min(3, 'name must be at least 3 characters.').nullable()
+        .transform((value) => value || null),
+    });
+
     const initialValues = {
       email: '',
       password: '',
@@ -41,27 +54,25 @@ export default {
       displayName: '',
     };
 
-    const schema = yup.object({
-      email: yup.string().required().email(),
-      password: yup.string().required().min(6),
-      confirmPassword: yup.string().required().oneOf([yup.ref('password')], 'password do not match.'),
-      displayName: yup.string().optional().min(30),
-    });
-
-    const { handleSubmit: submit, isSubmitting } = useForm({
+    const { handleSubmit: submit, resetForm } = useForm({
       validationSchema: schema,
       initialValues,
     });
 
     const handleSubmit = submit(async (data) => {
-      console.log(data);
+      ctx.emit('onSubmit', data);
     });
 
     const reset = () => {
-
+      resetForm(initialValues);
     };
 
-    return { isSubmitting, handleSubmit, reset };
+    return {
+      isSignUpSubmitting: computed(() => store.state.auth.isSignUpSubmitting),
+      isSubmitting: computed(() => store.getters['auth/isSubmitting']),
+      handleSubmit,
+      reset,
+    };
   },
 };
 </script>
