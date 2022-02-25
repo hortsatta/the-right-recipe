74a8f51260fa47f2db58745bb7a917f389ebb58d
Erwin Errol Calanuga <hortsatta@gmail.com>
2022-02-25 16:16:48 +0800

    Check user session on app load

--- src/App.vue
+++ src/App.vue
@@ -7,10 +7,20 @@
 </template>
 
 <script>
+import { useStore } from 'vuex';
+
+import { getAuthActionType } from '@/store/auth';
 import { TheHeader, TheFooter } from './components/core';
 
 export default {
   name: 'App',
   components: { TheHeader, TheFooter },
+  setup: () => {
+    const store = useStore();
+    // Check user session before create
+    if (typeof store.state.auth.isLoggedIn === 'undefined') {
+      store.dispatch(getAuthActionType(true).CHECK_USER_SESSION);
+    }
+  },
 };
 </script>
