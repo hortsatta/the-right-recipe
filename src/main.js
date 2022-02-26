0ea797ea443774d9b8eb50912ecd96653660ed6b
Erwin Errol Calanuga <hortsatta@gmail.com>
2022-02-27 01:05:15 +0800

    Check firebase auth before app mount

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 import { createApp } from 'vue';
 import PerfectScrollbar from 'vue3-perfect-scrollbar';
 import VueTippy from 'vue-tippy';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
 
 import App from './App.vue';
 import router from './router';
-import store from './store';
+import { store } from './store';
 import { buttonDirective } from './directives';
 import { globalComponents } from './includes';
 import './includes/firebase.include';
@@ -14,11 +15,16 @@ import 'vue3-perfect-scrollbar/dist/vue3-perfect-scrollbar.css';
 import 'tippy.js/dist/tippy.css';
 import './assets/css/main.css';
 
-createApp(App)
-  .use(store)
-  .use(router)
-  .use(globalComponents)
-  .use(PerfectScrollbar)
-  .use(VueTippy)
-  .directive('button', buttonDirective)
-  .mount('#app');
+let app;
+const auth = getAuth();
+onAuthStateChanged(auth, () => {
+  if (app) { return; }
+  app = createApp(App)
+    .use(store)
+    .use(router)
+    .use(globalComponents)
+    .use(PerfectScrollbar)
+    .use(VueTippy)
+    .directive('button', buttonDirective)
+    .mount('#app');
+});
